45a5b25bd06634436d19d14d4b5a5ca8100625ca
sussan24 <daniellanwenu@gmail.com>
2022-06-22 14:30:55 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,12 @@
+let fullName = 'Sussan Nwenu'
+let height = '5 feet 9 inches'
+let country = 'Nigeria'
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+
+document.getElementById("java1").innerHTML = "Susan Nwenu";
+document.getElementById("java2").innerHTML = "5 feet 9 inches";
+document.getElementById("java3").innerHTML = "Nigeria";
